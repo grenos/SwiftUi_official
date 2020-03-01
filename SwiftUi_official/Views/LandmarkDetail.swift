04bdd381f50382ae8e9cedcbce8797ff8a115b36
grenos <grenos>
2020-03-01 18:21:37 +0100

    Drawings

--- SwiftUi_official/Views/LandmarkDetail.swift
+++ SwiftUi_official/Views/LandmarkDetail.swift
@@ -72,6 +72,9 @@ struct LandmarkDetail: View {
     }
 }
 
+
+
+
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
